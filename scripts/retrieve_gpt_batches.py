fd4b5aefbb39bda54a9e844c8665072f316f1b45
Isabelle <lee.isabelle.g@gmail.com>
2024-12-21 21:33:06 +0000

    +

--- scripts/retrieve_gpt_batches.py
+++ scripts/retrieve_gpt_batches.py
@@ -7,19 +7,17 @@ client = OpenAI(api_key=api_key)
 total_batches = []
 batches = [x.id for x in client.batches.list(limit=100).data]
 
-
 while batches:
     total_batches += batches
     batches = [x.id for x in client.batches.list(after=batches[-1], limit=100).data]
-
 
 def retrieve_batches_and_save(batch_ids, filepath):
 
     files = [client.batches.retrieve(x).output_file_id for x in batch_ids]
     output_files = [
-        client.batches.retrieve(x).metadata["description"]
+        client.batches.retrieve(x).metadata["description"].replace(".jsonl","")
         + "-"
-        + client.batches.retrieve(x).input_file_id
+        + client.batches.retrieve(x).input_file_id.replace("file-","")
         + ".json"
         for x in batch_ids
     ]
